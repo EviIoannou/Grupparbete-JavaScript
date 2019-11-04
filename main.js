aff56ee347b27c372c922a66dffae9a059fffda6
Vimbayinashe <vimbayinashemandaza@gmail.com>
2019-11-04 14:39:46 +0100

    Added id value (equal to index value in res array)  to each station

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var res;
-var main = document.getElementById("molndalStations")
+var main = document.getElementById("molndalStations");
+var body = document.getElementsByTagName("body");
 
 fetch("http://data.goteborg.se/RiverService/v1.1/MeasureSites/66473147-1c20-40c1-b1f9-6d18f1e620bf?format=json")
 .then( response => {
@@ -14,6 +15,7 @@ console.log(res);
   })
 
 function render () {
+    let thisValue = 0;
 
     res.forEach(station => {
         let tapping = "No currently available data";
@@ -28,14 +30,71 @@ function render () {
 
         let div = document.createElement("div");
         div.classList.add("station");
+        div.setAttribute("id", thisValue);
         div.innerHTML = `<div>${station.Description} </div>
-                        <div>Flöde: ${tapping} </div>`
-        ;
+                        <div>Flöde: ${tapping} </div>`;
         main.appendChild(div);
+
         let button = document.createElement("button");
-      button.innerHTML = "See details";
-      div.appendChild(button)
-    
+        button.innerHTML = "See details";
+        div.appendChild(button)
+        thisValue++;
     });
 
+    // res.forEach(station => {
+    //     let tapping = "Ingen flöde data finns";
+
+    //     let div = document.createElement("div");
+    //     div.classList.add("station");
+    //     div.setAttribute("id", thisValue);
+    //     div.innerHTML = `<div id>${station.Description} </div>`;
+
+    //     let button = document.createElement("button");
+    //     button.innerHTML = "See details";
+
+    //     station.MeasureParameters.forEach(parameter => {
+    //         let info = document.createElement("div");
+
+    //         if (parameter.Code == "Tapping") {
+    //             tapping = "Flöde: " + parameter.CurrentValue;
+    //             // info.innerText = parameter.Description + ": " + tapping;
+    //             info.innerText = tapping;
+    //         } else {
+    //             info.classList.add("hidden");
+    //             info.innerText = parameter.Description + ": " + parameter.CurrentValue;
+    //         }
+
+    //         div.appendChild(info);
+    //     });
+        
+    //     console.log(tapping);
+
+
+    //     main.appendChild(div);
+    //     div.appendChild(button)
+    //     thisValue++;
+    // });
 }
+
+body[0].addEventListener("click", (e) => {
+
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.parentNode);
+
+    if (e.target.nodeName == "BUTTON") {
+        let index = e.target.parentNode;
+        
+
+
+    }
+
+
+
+
+
+
+
+
+
+})
